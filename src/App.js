1ecb46f58314d888f4da58681b206fb541d9bd75
Shaked Yunger <shakedyunger@gmail.com>
2022-06-25 19:20:41 +0300

    adding on chnage to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div>convertor-app</div>;
+  function handleChange(event) {
+    console.log(event.target.value);
+  }
+
+  return (
+    <>
+      <h1>convertor-app</h1>
+      <input name="firstName" onChange={handleChange} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Fahrenheit/F.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function F() {
+  return <div>F</div>;
+}
